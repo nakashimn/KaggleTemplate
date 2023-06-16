1ae17efe64ad45044ee3c73d5985d46a8a54b31a
nakashimn <nakashimn@gmail.com>
2023-06-17 02:29:30 +0900

    modify:format

--- src/components/datamodule.py
+++ src/components/datamodule.py
@@ -1,9 +1,11 @@
 import numpy as np
+from numpy.typing import NDArray
 import pandas as pd
 import cv2
 import librosa
 import torch
 from torch.nn import functional as F
+from torch.nn import Sequential
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms as Tv
 import albumentations as A
@@ -14,7 +16,12 @@ import traceback
 # For EfficientNetBaseModel(Image)
 ################################################################################
 class ImgDataset(Dataset):
-    def __init__(self, df, config, transform=None):
+    def __init__(
+            self,
+            df: pd.DataFrame,
+            config: dict,
+            transform: Tv.Compose | Sequential | None = None
+        ):
         self.config = config
         self.filepaths = self._read_filepaths(df)
         self.labels = None
@@ -29,7 +36,7 @@ class ImgDataset(Dataset):
     def __len__(self):
         return len(self.filepaths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         img = self._read_img(self.filepaths[idx])
         img = self.pre_transform(image=img)["image"]
         img = self.to_tensor(img)
@@ -40,19 +47,19 @@ class ImgDataset(Dataset):
             return img, labels
         return img
 
-    def _read_filepaths(self, df):
+    def _read_filepaths(self, df: pd.DataFrame):
         values = df["filepath"].values
         return values
 
-    def _read_img(self, filepath):
+    def _read_img(self, filepath: str):
         img = cv2.imread(filepath)
         return img
 
-    def _read_labels(self, df):
+    def _read_labels(self, df: pd.DataFrame):
         labels = torch.tensor(df.apply(self._to_onehot), dtype=torch.float32)
         return labels
 
-    def _to_onehot(self, series):
+    def _to_onehot(self, series: pd.Series):
         return [1 if l in series else 0 for l in self.config["labels"]]
 
 
@@ -60,7 +67,12 @@ class ImgDataset(Dataset):
 # For EfficientNetBaseModel(Audio)
 ################################################################################
 class AudioDataset(Dataset):
-    def __init__(self, df, config, transform=None):
+    def __init__(
+            self,
+            df: pd.DataFrame,
+            config: dict,
+            transform: Tv.Compose | Sequential | None = None
+        ):
         self.config = config
         self.filepaths = self._read_filepaths(df)
         self.labels = None
@@ -75,7 +87,7 @@ class AudioDataset(Dataset):
     def __len__(self):
         return len(self.filepaths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         melspec = self._read_melspec(self.filepaths[idx])
         melspec = self._normalize(melspec)
         melspec = self.pre_transform(image=melspec)["image"]
@@ -87,16 +99,16 @@ class AudioDataset(Dataset):
             return melspec, labels
         return melspec
 
-    def _read_filepaths(self, df):
+    def _read_filepaths(self, df: pd.DataFrame):
         values = df["filepath"].values
         return values
 
-    def _read_melspec(self, filepath):
+    def _read_melspec(self, filepath: str):
         melspec = np.load(filepath)["arr_0"]
         melspec = np.expand_dims(melspec, axis=-1)
         return melspec
 
-    def _normalize(self, melspec, eps=1e-6):
+    def _normalize(self, melspec: NDArray, eps: float = 1e-6):
         melspec = (melspec - melspec.mean()) / (melspec.std() + eps)
         if (melspec.max() - melspec.min()) < eps:
             return np.zeros_like(melspec, dtype=np.uint8)
@@ -105,18 +117,26 @@ class AudioDataset(Dataset):
         ).astype(np.uint8)
         return melspec
 
-    def _read_labels(self, df):
+    def _read_labels(self, df: pd.DataFrame):
         labels = torch.tensor(df.apply(self._to_onehot), dtype=torch.float32)
         return labels
 
-    def _to_onehot(self, series):
+    def _to_onehot(self, series: pd.Series):
         return [1 if l in series else 0 for l in self.config["labels"]]
 
 ################################################################################
 # DataModule
 ################################################################################
 class DataModule(LightningDataModule):
-    def __init__(self, df_train, df_val, df_pred, Dataset, config, transforms):
+    def __init__(
+            self,
+            df_train: pd.DataFrame | None,
+            df_val: pd.DataFrame | None,
+            df_pred: pd.DataFrame | None,
+            Dataset: Dataset,
+            config: dict,
+            transforms: Tv.Compose | Sequential | None
+        ):
         super().__init__()
 
         # const
@@ -129,7 +149,7 @@ class DataModule(LightningDataModule):
         # class
         self.Dataset = Dataset
 
-    def _read_transforms(self, transforms):
+    def _read_transforms(self, transforms: Tv.Compose | Sequential | None):
         if transforms is not None:
             return transforms
         return {"train": None, "valid": None, "pred": None}
